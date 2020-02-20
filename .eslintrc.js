e5968d7ddcbffba0d58bbf77e2166bb7ec76fcc9
danielsilva2017 <silvadaniel@ua.pt>
2020-02-20 14:42:32 +0000

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: false,
+  env: {
+    browser:false,
+    node: false
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :data="data" @changeData="changeData($event)"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component,Vue,Prop } from 'vue-property-decorator'
+import HelloWorld from './components/HelloWorld.vue'
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class App extends Vue {
+  @Prop() private data = ''
+  changeData(data:any){
+    this.data=data;
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
